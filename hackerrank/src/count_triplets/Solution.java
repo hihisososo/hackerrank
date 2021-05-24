2107062ef957d068e74a6e42b810903168003923
lyj <hihisososo@naver.com>
2021-05-25 08:38:57 +0900

    count triperts 중간풀이

--- hackerrank/src/count_triplets/Solution.java
+++ hackerrank/src/count_triplets/Solution.java
@@ -8,7 +8,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -16,6 +18,8 @@ public class Solution {
 
 	// Complete the countTriplets function below.
 	static long countTriplets(List<Long> arr, long r) {
+		Collections.sort(arr);
+		HashSet<Long> sets = new HashSet<>();
 		HashMap<Long, ArrayList<Integer>> map = new HashMap<>();
 		for (int i = 0; i < arr.size(); i++) {
 			long number = arr.get(i);
@@ -24,29 +28,30 @@ public class Solution {
 			}
 			ArrayList<Integer> idxs = map.get(number);
 			idxs.add(i);
+			sets.add(number);
 		}
-		int cnt = 0;
-		for (int i = 0; i < arr.size(); i++) {
-			long number = arr.get(i);
-			ArrayList<Integer> idxs1 = map.get(number * r);
-			if (idxs1 != null) {
-				for (int j = 0; j < idxs1.size(); j++) {
-					if (idxs1.get(j) > i) {
-						ArrayList<Integer> idxs2 = map.get(number * r * r);
-						if (idxs2 != null) {
-							for (int k = 0; k < idxs2.size(); k++) {
-								if (idxs2.get(k) > j) {
-									cnt++;
-								}
-							}
-						}
-					}
+
+		long cnt = 0;
+		for (Long number : sets) {
+			
+			if(r == 1){
+				ArrayList<Integer> idxs1 = map.get(number);
+				if(idxs1.size() >= 3){
+					cnt += (((long)idxs1.size() * ((long)idxs1.size() - 1) * ((long)idxs1.size() - 2)) / 6);
 				}
+			}else{
+			ArrayList<Integer> idxs1 = map.get(number);
+			ArrayList<Integer> idxs2 = map.get(number * r);
+			ArrayList<Integer> idxs3 = map.get(number * r * r);
+			if(idxs1 != null && idxs2 != null && idxs3 != null)
+				cnt += (long)idxs1.size() * (long)idxs2.size() * (long)idxs3.size();
 			}
 		}
+		System.out.println(cnt);
 		return cnt;
 
 	}
+
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
